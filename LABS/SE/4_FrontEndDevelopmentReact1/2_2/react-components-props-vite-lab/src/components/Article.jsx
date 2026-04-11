3b25226d1b1df4734037c36e1e7c31d2ffeab3f2
Allison Valdez <allison.snipe@gmail.com>
2026-04-10 20:55:07 -0700

    done check

---
+++ LABS/SE/4_FrontEndDevelopmentReact1/2_2/react-components-props-vite-lab/src/components/Article.jsx
@@ -0,0 +1,16 @@
+// Import React
+import React from "react";
+
+// Create the Article component
+function Article() {
+    return (
+        <article>
+            <h3>{title}</h3>
+            <small>{date}</small>
+            <p>{preview}</p>
+        </article>
+    );
+}
+
+// Export to make it globally available
+export default Article;
